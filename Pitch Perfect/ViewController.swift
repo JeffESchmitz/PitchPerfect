f540f4d8ba11a76299f7a44f999371dca288985e
Jeff Schmitz <jeffeschmitz@gmail.com>
2015-12-06 08:57:46 -0600

    Lesson 3a completed

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let backgroundLightBlue = UIColor(red: 0.94, green: 1.00, blue: 1.00, alpha: 1.0)
+    let backgroundLightRed = UIColor(red: 1.00, green: 0.89, blue: 0.89, alpha: 1.0)
+    
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +27,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        stopButton.hidden = true
+        recordButton.enabled = true
+    }
     @IBAction func recordAudio(sender: UIButton) {
-        recordingInProgress.hidden = false
         print("in recordAudio")
+        changeViewBackgroundColor(backgroundLightRed)
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
     }
 
     @IBAction func stopRecording(sender: UIButton) {
+        changeViewBackgroundColor(backgroundLightBlue)
         recordingInProgress.hidden = true
+        recordButton.enabled = true
+    }
+
+    func changeViewBackgroundColor(color: UIColor) -> Void {
+        UIView.animateWithDuration(0.25, delay: 0, options: UIViewAnimationOptions.AllowUserInteraction, animations: {
+            self.view.backgroundColor = color
+            }, completion: nil)
+
     }
 }
 
