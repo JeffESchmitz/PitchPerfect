09eab42ff270c81ebd5815e173e09ccc0fda3b9d
Jeff Schmitz <jeffeschmitz@gmail.com>
2015-12-07 10:01:42 -0600

    Lesson 3b completed

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,61 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Jeff Schmitz on 12/6/15.
+//  Copyright © 2015 Jeff Schmitz. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let filePath = NSBundle.mainBundle().URLForResource("movie_quote", withExtension: "mp3") {
+            audioPlayer = try! AVAudioPlayer(contentsOfURL: filePath)
+        } else {
+            print("the filePath is empty")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlowAudio(sender: UIButton) {
+        playAudio(0.5)
+    }
+
+    @IBAction func playFastAudio(sender: UIButton) {
+        playAudio(1.5)
+    }
+    
+    @IBAction func stopPlayingAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
+    
+    func playAudio(rate: Float) -> Void {
+        audioPlayer.stop()
+        audioPlayer.enableRate = true
+        audioPlayer.rate = rate
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
